76157c51931092f1a008d23287b726f1144a5349
IndianShogun1 <shreyansh9383@gmail.com>
2018-03-16 10:07:34 +0530

    Update TransactionConfig.java

--- transaction/src/main/java/Q1_and_Q2/TransactionConfig.java
+++ transaction/src/main/java/Q1_and_Q2/TransactionConfig.java
@@ -29,7 +29,7 @@ return jdbcTemplate;
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/transactionDemo");
         dataSource.setUsername("root");
-        dataSource.setPassword("shreyansh");
+        dataSource.setPassword("test");
         return dataSource;
     }
 
